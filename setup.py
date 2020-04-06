2f838e9e81ebdd58087b31ccfdf3b78227a1b2c7
vonum <vonum.mk@gmail.com>
2020-04-06 16:54:34 +0200

    Project setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="multi-prophet",
+    version="0.1",
+    author="Milan Keca",
+    author_email="vonum.mk@gmail.com",
+    description="Multivariate forecasting using Facebook Prophet",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vonum/multi-prophet",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
