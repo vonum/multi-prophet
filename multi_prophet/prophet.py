2ca39f95a1046c8e9c656eb84eea7c4719a931d9
vonum <vonum.mk@gmail.com>
2020-04-06 18:21:07 +0200

    Basic class wrapper

---
+++ multi_prophet/prophet.py
@@ -0,0 +1,3 @@
+class Prophet:
+    def __init__(self):
+        pass
--- tests/test_multi_prophet.py
+++ tests/test_multi_prophet.py
@@ -6,3 +6,6 @@ class MultiProphetTestCase(unittest.TestCase):
 
     def test_version(self):
         self.assertEqual("0.1", multi_prophet.__version__)
+
+    def test_constructor(self):
+        self.assertIsNotNone(multi_prophet.Prophet())
