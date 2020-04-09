0c6c963c2f0e3cda872d15145a5976fdff5dd942
vonum <vonum.mk@gmail.com>
2020-04-09 09:37:26 +0200

    Initial multi model support

--- multi_prophet/__init__.py
+++ multi_prophet/__init__.py
@@ -1,3 +1,42 @@
+import pandas as pd
 from .prophet import Prophet
 
 __version__ = "0.1"
+TIME_COLUMN = "ds"
+
+
+class MultiProphet:
+
+    def __init__(self, columns, **kwargs):
+        self.model_pool = self._init_model_pool(columns, **kwargs)
+
+    def fit(self, df, **kwargs):
+        for column, model in self.model_pool.items():
+            mdf = self._create_dataframe(df, column)
+            model.fit(mdf, **kwargs)
+
+    def make_future_dataframe(self, periods):
+        model = self._first_model()
+        return model.make_future_dataframe(periods)
+
+    def predict(self, future_df):
+        return {
+            column: model.predict(future_df)
+            for column, model in self.model_pool.items()
+        }
+
+    def add_seasonality(self, **kwargs):
+        for model in self.model_pool.values():
+            model.add_seasonality(**kwargs)
+
+    def _init_model_pool(self, columns, **kwargs):
+        return {c: Prophet(**kwargs) for c in columns}
+
+    def _first_model(self):
+        return list(self.model_pool.values())[0]
+
+    def _create_dataframe(self, df, column):
+        return pd.DataFrame({
+          "ds": df[TIME_COLUMN].values,
+          "y": df[column].values
+        })
---
+++ tests/test_prophet.py
@@ -0,0 +1,96 @@
+import unittest
+import numpy as np
+import pandas as pd
+import fbprophet
+import multi_prophet
+
+
+class ProphetTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.df = pd.read_csv("tests/data/example_wp_log_peyton_manning.csv")
+
+    def test_constructor(self):
+        mp = multi_prophet.Prophet()
+        self.assertIsNotNone(mp)
+        self.assertIsInstance(mp.prophet, fbprophet.Prophet)
+
+    def test_constructor_kwargs(self):
+        mp = multi_prophet.Prophet(growth="logistic")
+        self.assertEqual("logistic", mp.prophet.growth)
+
+    def test_constructor_invalid_kwargs(self):
+        with self.assertRaises(TypeError):
+            mp = multi_prophet.Prophet(invalid="logistic")
+
+    def test_fit(self):
+        mp = multi_prophet.Prophet()
+        mp.fit(self.df)
+        # will be tested later by checking the performance of the model
+        self.assertTrue(True)
+
+    def test_fit_kwargs(self):
+        mp = multi_prophet.Prophet()
+        mp.fit(self.df, algorithm="Newton")
+        # will be tested later by checking the performance of the model
+        self.assertTrue(True)
+
+    def test_fit_invalid_kwargs(self):
+        mp = multi_prophet.Prophet()
+        with self.assertRaises(ValueError):
+            mp.fit(self.df, invalid="Newton")
+
+    def test_make_future_dataframe_length(self):
+        mp = multi_prophet.Prophet()
+        mp.fit(self.df)
+        future_df = mp.make_future_dataframe(7)
+
+        initial_len = len(self.df)
+
+        self.assertEqual(initial_len + 7, len(future_df))
+
+    def test_make_future_dataframe_dates(self):
+        dates = [
+            "2016-01-21", "2016-01-22", "2016-01-23", "2016-01-24",
+            "2016-01-25", "2016-01-26", "2016-01-27"
+        ]
+
+        mp = multi_prophet.Prophet()
+        mp.fit(self.df)
+        future_df = mp.make_future_dataframe(7)
+
+        future_dates = [
+            np.datetime_as_string(d, unit="D")
+            for d in future_df.tail(7)["ds"].values
+        ]
+
+        self.assertEqual(dates, future_dates)
+
+    def test_predict(self):
+        columns = [
+            "ds", "trend", "yhat_lower", "yhat_upper",
+            "trend_lower", "trend_upper", "additive_terms", "additive_terms_lower",
+            "additive_terms_upper", "weekly", "weekly_lower", "weekly_upper",
+            "yearly", "yearly_lower", "yearly_upper", "multiplicative_terms",
+            "multiplicative_terms_lower", "multiplicative_terms_upper", "yhat"
+        ]
+
+        mp = multi_prophet.Prophet()
+        mp.fit(self.df)
+        future_df = mp.make_future_dataframe(7)
+
+        forecast = mp.predict(future_df)
+
+        self.assertEqual(len(future_df), len(forecast))
+        self.assertEqual(columns, list(forecast.columns.values))
+
+    def test_add_seasonality(self):
+        mp = multi_prophet.Prophet()
+        mp.add_seasonality(name="monthly", period=30.5, fourier_order=5)
+        seasonality = mp.prophet.seasonalities["monthly"]
+
+        self.assertEqual(30.5, seasonality["period"])
+        self.assertEqual(5, seasonality["fourier_order"])
+        self.assertEqual(10, seasonality["prior_scale"])
+        self.assertEqual("additive", seasonality["mode"])
+        self.assertIsNone(seasonality["condition_name"])
