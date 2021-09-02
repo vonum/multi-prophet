aca9b17f8255770afababc3f2cebfcd99a2cd82e
vonum <vonum.mk@gmail.com>
2021-09-02 20:12:42 +0200

    Bump to version 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="multi-prophet",
-    version="0.3.0",
+    version="1.0.0",
     author="Milan Keca",
     author_email="vonum.mk@gmail.com",
     description="Multivariate forecasting using Facebook Prophet",
